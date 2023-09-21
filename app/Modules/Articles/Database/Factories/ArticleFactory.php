b6438f5e1926dd8ef7e838c552dd580704ec018f
Solvi <solvik74@mail.ru>
2023-09-22 02:49:02 +0500

    Move factory inside module

--- app/Modules/Articles/Database/Factories/ArticleFactory.php
+++ app/Modules/Articles/Database/Factories/ArticleFactory.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace Database\Factories;
+namespace App\Modules\Articles\Database\Factories;
 
+use App\Modules\Articles\Models\Article;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Modules\Articles\Models\Article>
  */
 class ArticleFactory extends Factory
 {
+    protected $model = Article::class;
+
     /**
      * Define the model's default state.
      *
--- app/Modules/Articles/Models/Article.php
+++ app/Modules/Articles/Models/Article.php
@@ -6,6 +6,7 @@ namespace App\Modules\Articles\Models;
 
 use App\Common\Models\BaseModel;
 use App\Common\Traits\Models\HasElasticsearch;
+use App\Modules\Articles\Database\Factories\ArticleFactory;
 use App\Modules\Articles\Observers\ArticleElasticsearchObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -16,6 +17,11 @@ class Article extends BaseModel
     protected $casts = [
         'tags' => 'json',
     ];
+
+    protected static function newFactory()
+    {
+        return new ArticleFactory;
+    }
 
     public static function getSearchObserver()
     {
